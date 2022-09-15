e2422fcae874e1cca92c41d8c46e30c6eaddec26
Chewy-X <112655506+Chewy-X@users.noreply.github.com>
2022-09-15 15:09:04 -0400

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { OllieMainComponent } from './ollie-main/ollie-main.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      { path: 'bikeshop', component: OllieMainComponent }
     ])
   ],
   declarations: [
